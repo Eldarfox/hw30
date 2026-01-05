4b42da92eb078a4a2732e2211e4da3ac8bb83a4e
Eldarfox <eldarkemelov200@gmail.com>
2026-01-05 18:11:27 +0600

    sorted customer emails

--- src/RestaurantOrders.java
+++ src/RestaurantOrders.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class RestaurantOrders {
@@ -80,6 +81,14 @@ public class RestaurantOrders {
         return orders.stream()
                 .mapToDouble(Order::getTotal)
                 .sum();
+    }
+
+    public List<String> uniqueSortedEmails() {
+        return orders.stream()
+                .map(o -> o.getCustomer().getEmail())
+                .collect(Collectors.toCollection(TreeSet::new))
+                .stream()
+                .collect(Collectors.toList());
     }
 
 }
